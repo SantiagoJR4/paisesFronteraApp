4fe96d24616c762fa3d3375d56cbb5c03cc2608c
santiagoRosero <santipo12@gmail.com>
2022-12-09 10:25:46 -0500

    regiones cargadas

---
+++ src/app/paises/pages/selector-pages/selector-pages.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
+
+@Component({
+  selector: 'app-selector-pages',
+  templateUrl: './selector-pages.component.html',
+  styleUrls: ['./selector-pages.component.css']
+})
+export class SelectorPagesComponent implements OnInit {
+
+  miFormulario:FormGroup=this.fb.group({
+    region:['',[Validators.required]]
+  })
+
+  //llenar selectores
+  regiones:string[]=[];
+
+
+  constructor( private fb:FormBuilder, private paisesService:PaisesService) { }
+
+  ngOnInit(): void {
+    this.regiones=this.paisesService.regiones;
+  }
+
+  guardar(){
+
+  }
+
+}
