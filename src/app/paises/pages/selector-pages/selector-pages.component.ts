b029dc944dcf41d55194ac6efedad77cceae09c2
santiagoRosero <santipo12@gmail.com>
2022-12-12 10:28:45 -0500

    segundo selector

--- src/app/paises/pages/selector-pages/selector-pages.component.ts
+++ src/app/paises/pages/selector-pages/selector-pages.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { switchMap, tap } from 'rxjs';
+import { PaisSmall } from '../../interfaces/paises.interface';
 import { PaisesService } from '../../services/paises.service';
 
 @Component({
@@ -10,17 +12,52 @@ import { PaisesService } from '../../services/paises.service';
 export class SelectorPagesComponent implements OnInit {
 
   miFormulario:FormGroup=this.fb.group({
-    region:['',[Validators.required]]
+    region:['',[Validators.required]],
+    pais:['',[Validators.required]],
+    frontera:['',[Validators.required]]
   })
 
   //llenar selectores
   regiones:string[]=[];
+  paises:PaisSmall[]=[];
 
 
   constructor( private fb:FormBuilder, private paisesService:PaisesService) { }
 
   ngOnInit(): void {
     this.regiones=this.paisesService.regiones;
+
+    //cuando cambie la region
+    // this.miFormulario.get('region')?.valueChanges
+    //   .subscribe(region =>{
+    //     console.log(region);
+
+    //     this.paisesService.getPaisesPorRegion(region)
+    //       .subscribe(paises=>{
+    //         console.log(paises);
+    //         this.paises=paises;
+    //       });
+    //   })
+
+    //cuando cambie la region
+    this.miFormulario.get('region')?.valueChanges
+      .pipe(
+        tap((_)=>{
+          this.miFormulario.get('pais')?.reset('');
+        }),
+        switchMap(region => this.paisesService.getPaisesPorRegion(region))
+      )
+      .subscribe(paises=>{
+        this.paises=paises;
+      });
+
+    //cuando cambie el pais
+    this.miFormulario.get('pais')?.valueChanges
+      .subscribe(codigo=>{
+        console.log(codigo);
+        
+      })
+
   }
 
   guardar(){
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Country } from '../interfaces/country.interface';
+import { PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +11,30 @@ export class PaisesService {
 
   private _regiones:string[]=['Africa','Americas','Asia','Europe','Oceania'];
 
+  private baseUrl:string='https://restcountries.com/v3.1';
+
+
   get regiones() : string[]{
     return [...this._regiones]
   }
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getPaisesPorRegion(region:string): Observable<PaisSmall[]>{
+
+    const url:string=`${this.baseUrl}/region/${region}?fields=name,cca3`
+    return this.http.get<PaisSmall[]>(url);
+  }
+
+  getPaisPorCodigo(codigo:string):Observable<Country | null>{
+
+    if(!codigo){
+      return of(null)
+    }
+
+    const url = `${this.baseUrl}/alpha/${codigo}`;
+    return this.http.get<Country>(url);
+  }
+
+
 }
